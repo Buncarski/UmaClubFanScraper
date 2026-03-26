48d4703833e1d21c90407d3009339130ba0419f3
Buncarski <54981292+Buncarski@users.noreply.github.com>
2026-03-26 16:51:10 +0100

    first commit

---
+++ src/Classes/UmaClubFanScraper.cs
@@ -0,0 +1,8 @@
+public class UmaClubFanScraper
+{
+    public Dictionary<string, long> trainers = new Dictionary<string, long>();
+    public void Run()
+    {
+        Console.WriteLine("Beginning Scraping of fans for the desired uma club.");
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,9 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        UmaClubFanScraper umaClubFanScraper = new UmaClubFanScraper();
+
+        umaClubFanScraper.Run();
+    }
+}
