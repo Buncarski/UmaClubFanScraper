4393bd19032ec0f598740b28d0aec53cfd0779cc
Buncarski <54981292+Buncarski@users.noreply.github.com>
2026-04-06 20:36:17 +0200

    Drop files from .gitignore

--- bin/Debug/net8.0/Sources/Data.cs
+++
@@ -1,7 +0,0 @@
-public static class Data
-{
-    public static string ChronoGenesisUrl = "https://chronogenesis.net/api/club_profile?circle_id=SBC%202";
-    public static string UmaMoeUrl = "https://uma.moe/api/v4/circles?circle_id={0}&year={1}&month={2}";
-    public static string clubTrainerNamesFilePath = "Sources\\Clubs\\{0}.txt";
-    public static string outputPath = "Output\\{0}.txt";
-}
